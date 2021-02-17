d1159f565420c44e57442c373eeb3bb20c278d0b
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-17 17:02:59 -0300

    Rails Simple Airbnb Exercise Complete

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,48 @@
+class FlatsController < ApplicationController
+  before_action :find, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find
+    @flat = Flat.find(params[:id])
+  end
+end
